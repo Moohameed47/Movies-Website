a30e4fa9a2a4a4833e42e848b5a3a032c5ca2906
Moohameed47 <d.mohamedtarek12@gmail.com>
2023-12-24 15:03:39 +0200

    Get API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const API = "https://api.themoviedb.org/3/discover/movie?api_key=1907dd7e22213c1275b820c5455946aa&page=2&sort_by=popularity.desc";
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
