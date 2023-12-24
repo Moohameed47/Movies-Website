5717e0ca133010ac8d0aed682afe4595e02c0732
Moohameed47 <d.mohamedtarek12@gmail.com>
2023-12-24 15:31:33 +0200

    Add SpliderJs-FontAwesome6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
-const API = "https://api.themoviedb.org/3/discover/movie?api_key=1907dd7e22213c1275b820c5455946aa&page=2&sort_by=popularity.desc";
+const API =
+    'https://api.themoviedb.org/3/discover/movie?api_key=1907dd7e22213c1275b820c5455946aa&page=2&sort_by=popularity.desc';
 
 function App() {
-  const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(API)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setMovies(data.results)
-      })
-  }, [])
+    useEffect(() => {
+        fetch(API)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setMovies(data.results);
+            });
+    }, []);
 
-  return (
-    <>
-
-    </>
-  );
+    return <></>;
 }
 
 export default App;
